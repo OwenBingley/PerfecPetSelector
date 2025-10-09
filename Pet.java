c01706acbb4716cebc899160c440cad8e485dd6e
OwenBingley <owenbingley10@gmail.com>
2025-10-09 15:03:06 +0000

    save 1

--- Pet.java
+++ Pet.java
@@ -1,1 +1,35 @@
+public class Main {
+public static void main(String[] args) {
 
+String color = blue;
+String season = fall;
+
+
+If (color == blue && season == fall)
+
+else If
+
+
+else If
+
+
+else if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
