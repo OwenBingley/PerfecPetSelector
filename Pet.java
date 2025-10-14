c67c6dac4499424b03be77a7db19fce0e73d3042
OwenBingley <owenbingley10@gmail.com>
2025-10-14 14:55:07 +0000

    s

--- Pet.java
+++ Pet.java
@@ -1,61 +1,78 @@
-import java.util.*;
-public class Main {
+public class PetAssigner {
+
+
+public static String assignPet(String name, String colorPreference, String favoriteSeason) {
+
+
+String pet = "pet rock"; // Default pet
+
+
+boolean nameStartsWithConsonant = !name.matches("^[AEIOUaeiou].*");
+
+
+if ("blue".equalsIgnoreCase(colorPreference)) {
+if ("fall".equalsIgnoreCase(favoriteSeason)) {
+
+
+pet = "alligator";
+} else if ("spring".equalsIgnoreCase(favoriteSeason)) {
+
+
+pet = "ostrich";
+} else if (nameStartsWithConsonant && !"summer".equalsIgnoreCase(favoriteSeason) && !"fall".equalsIgnoreCase(favoriteSeason)) {
+
+
+pet = "axolotl";
+
+} else if ("green".equalsIgnoreCase(colorPreference)) {
+
+
+if (nameStartsWithConsonant && "winter".equalsIgnoreCase(favoriteSeason)) {
+pet = "giraffe";
+
+
+} else if (!"fall".equalsIgnoreCase(favoriteSeason)) {
+pet = "dog";
+}
+
+
+} else if ("red".equalsIgnoreCase(colorPreference)) {
+if (name.matches("^[AEIOUaeiou].*")) {
+
+
+pet = "porcupine";
+} else {
+pet = "panda";
+}
+
+
+}
+
+
+if ("summer".equalsIgnoreCase(favoriteSeason)) {
+if (!"dog".equals(pet) && !"panda".equals(pet) && !"porcupine".equals(pet)) {
+
+
+pet = "pony";
+}
+}
+
+
+return pet;
+}
+
+
 public static void main(String[] args) {
 
-String color = "blue";
-String season = "fall";
-string name = "owen";
-string pet;
-
-If (color == "blue" && season == "fall"); { 
-pet = "alligator";
-}
-
- If (color == "blue" && season == "spring");{
-pet = "ostrich";
-
-} 
-
- If (color == "green" && season == "winter" && ("abcdefghijklmnopqrstuvwxyz".indexOf(name.substring(i, i + 1).toLowerCase()) >= 0)); { 
-pet = "girafe";
 
 
-}
-
-if (color == "green" && season != "fall"); {
-else if (pet == "girafe");{
-pet = "girage";
-}
-else{
-   pet = "dog" ;
-}
- 
-}
+String name = "Owen";
+String colorPreference = "blue";
+String favoriteSeason = "spring";
 
 
-
-
- if (color == red ); {
-pet ="panda";
- 
-}
-
-
-
-
-
-
-}
-
-
-}
-
-
-
-
-
-
-
+String assignedPet = assignPet(name, colorPreference, favoriteSeason);
+System.out.println("Assigned pet: " + assignedPet);
 
 
 }
